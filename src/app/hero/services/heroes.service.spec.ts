ff746bc77db63d6ee3eb4a0296ca9922937a9f3c
denirjr <denirdamasceno7@gmail.com>
2024-06-10 23:37:08 -0300

    doc(): add unit test and documentation

--- src/app/hero/services/heroes.service.spec.ts
+++ src/app/hero/services/heroes.service.spec.ts
@@ -1,16 +1,40 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, fakeAsync, tick, waitForAsync } from '@angular/core/testing';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HeroModel } from '../models/hero-model';
 import { HeroesService } from './heroes.service';
+import { EMPTY, of } from 'rxjs';
 
 describe('HeroesService', () => {
   let service: HeroesService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(HeroesService);
-  });
+  }));
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('Should search', fakeAsync(() => {
+    const spy = spyOn(service, 'search').and.returnValue(EMPTY);
+
+    service.search('thor').subscribe((res) => {
+      expect(res).toBe(spy);
+    });
+
+    tick();
+  }));
+
+  it('Should search by id', fakeAsync(() => {
+    const spy = spyOn(service, 'searchById').and.callThrough();
+
+    service.searchById('1').subscribe(() => {
+      expect(spy).toHaveBeenCalled();
+    });
+    tick();
+  }));
 });
--- src/app/pipes/format-img.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FormatImgPipe } from './format-img.pipe';
-
-describe('FormatImgPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FormatImgPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
